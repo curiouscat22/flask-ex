0f13a4a2c45862252fd7f314269debef73008846
Hariprakash Reddy <33058608+hreddy11@users.noreply.github.com>
2022-04-06 18:04:07 +0530

    added few files

--- api/db.py
+++ api/db.py
@@ -2,6 +2,34 @@ from psycopg2 import pool
 from psycopg2.extras import RealDictCursor
 import logging
 
+# -----
+import psycopg2
+from sqlalchemy import create_engine
+import csv
+
+
+engine = create_engine('postgresql+psycopg2://postgres:Testing123@dbservice:5432/postgres')
+conn = engine.connect()
+conn.execute("commit")
+conn.execute("create database test")
+conn.execute("commit")
+conn.close()
+
+# copy csv to postgres
+conn = psycopg2.connect(database="test", user="postgres", password="Testing123", host="dbservice", port="5432")
+cursor = conn.cursor()
+
+# create table
+cursor.execute("CREATE TABLE IF NOT EXISTS countries (id serial PRIMARY KEY, country varchar(200), alpha2 char(30), alpha3 char(30), nuCode int, latitude float, longitude float)")
+# copy data from csv to table
+with open('countries.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        cursor.execute("INSERT INTO countries (country, alpha2, alpha3, nuCode, latitude, longitude) VALUES (%s, %s, %s, %s, %s, %s)", (row['Country'], row['Alpha2_code'], row['Alpha3_code'], row['Numeric_code'], row['Latitude'], row['Longitude']))
+conn.commit()
+conn.close()
+# ---
+
 try: 
     connection_pool = pool.SimpleConnectionPool(1, 5, host="dbservice", port=5432,
                                                 database="test",
@@ -10,13 +38,11 @@ try:
 except Exception as e:
     connection_pool = None
     logging.info(str(e))
-
-
 """
 Queries 
-"SELECT * from counties limit 10;"
+"SELECT * from countries limit 10;"
 
-"SELECT * from counties WHERE id = {id}
+"SELECT * from countries WHERE id = {id}
 """
 
 def get_top_10():
@@ -24,7 +50,7 @@ def get_top_10():
         conn = connection_pool.getconn()
         values = None
         with conn.cursor(cursor_factory=RealDictCursor) as cursor:
-            cursor.execute("SELECT * from counties limit 10;")
+            cursor.execute("SELECT * from countries limit 10;")
             values = cursor.fetchall()
         connection_pool.putconn(conn)
         return values
@@ -35,7 +61,7 @@ def get_one_country(id_):
         conn = connection_pool.getconn()
         values = None
         with conn.cursor(cursor_factory=RealDictCursor) as cursor:
-            values = cursor.execute(f"SELECT * from counties WHERE id = {id_}")
+            values = cursor.execute(f"SELECT * from countries WHERE id = {id_}")
             values = cursor.fetchall()
         connection_pool.putconn(conn)
         return values
---
+++ api/main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from db import get_one_country, get_top_10
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    top_10 = json.dumps(get_top_10())
+    return top_10
+
+@app.route("/<id>")
+def hello_world_one(id):
+    one_c = json.dumps(get_one_country(id_=id))
+    return one_c
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
